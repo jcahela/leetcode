c1ec12ddf9d82ec0c9a5c7eb2647145fa5bf1de3
jcahela <jpacahela@gmail.com>
2023-08-14 17:54:37 -0500

    Add pre-ES6 class practice car example, using functions and prototype for instance variables and methods

--- class_practice.js
+++ class_practice.js
@@ -1,5 +1,45 @@
+/* --------------------------- Pre-ES6 Class Construction --------------------------------------- */
+
+// Class, with constructor
+function Car(make, model, year, miles = 0) {
+  // Constructor: Instance variables
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.miles = miles;
+
+  // Increment the Static Variable numCars when this class is instantiated
+  Car.numCars += 1;
+}
+
+// Static variable
+Car.numCars = 0;
+
+// Instance method
+Car.prototype.drive = function(distance) {
+  this.miles += distance;
+}
+
+// Static method
+Car.getNumCars = function() {
+  return Car.numCars;
+}
+
+const myNewCar = new Car('Nissan', 'Sentra', 2014);
+
+// Instance variables
+console.log(myNewCar.make); // output: 'Nissan'
+console.log(myNewCar.model); // output: 'Sentra'
+console.log(myNewCar.year); // output: 2014
+console.log(myNewCar.miles); // output: 0
+// Instance method (on instance/instantiated object)
+myNewCar.drive(100);
+// Instance variable
+console.log(myNewCar.miles); // output: 100
+// Static method (on Car class, static, stays the same)
+console.log(Car.getNumCars()); // output: 1
+
 /* --------------------------- ES6 Class Construction --------------------------------------- */
 
 
-/* --------------------------- Pre-ES6 Class Construction --------------------------------------- */
-
+/* --------------------------- TypeScript Class Construction --------------------------------------- */
