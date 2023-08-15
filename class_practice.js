0cc3eaabc2d5eb96d701db920c1c5a9911935d4b
jcahela <jpacahela@gmail.com>
2023-08-15 16:07:11 -0500

    Add class practice ES6 classes using class keyword

--- class_practice.js
+++ class_practice.js
@@ -1,31 +1,72 @@
 /* --------------------------- Pre-ES6 Class Construction --------------------------------------- */
 
-// Class, with constructor
-function Car(make, model, year, miles = 0) {
-  // Constructor: Instance variables
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  this.miles = miles;
+// // Class, with constructor
+// function Car(make, model, year, miles = 0) {
+//   // Constructor: Instance variables
+//   this.make = make;
+//   this.model = model;
+//   this.year = year;
+//   this.miles = miles;
 
-  // Increment the Static Variable numCars when this class is instantiated
-  Car.numCars += 1;
-}
+//   // Increment the Static Variable numCars when this class is instantiated
+//   Car.numCars += 1;
+// }
 
-// Static variable
-Car.numCars = 0;
+// // Static variable
+// Car.numCars = 0;
 
-// Instance method
-Car.prototype.drive = function(distance) {
-  this.miles += distance;
-}
+// // Instance method
+// Car.prototype.drive = function(distance) {
+//   this.miles += distance;
+// }
 
-// Static method
-Car.getNumCars = function() {
-  return Car.numCars;
+// // Static method
+// Car.getNumCars = function() {
+//   return Car.numCars;
+// }
+
+// const myNewCar = new Car('Nissan', 'Sentra', 2014);
+
+// // Instance variables
+// console.log(myNewCar.make); // output: 'Nissan'
+// console.log(myNewCar.model); // output: 'Sentra'
+// console.log(myNewCar.year); // output: 2014
+// console.log(myNewCar.miles); // output: 0
+// // Instance method (on instance/instantiated object)
+// myNewCar.drive(100);
+// // Instance variable
+// console.log(myNewCar.miles); // output: 100
+// // Static method (on Car class, static, stays the same)
+// console.log(Car.getNumCars()); // output: 1
+
+/* --------------------------- ES6 Class Construction --------------------------------------- */
+
+class Car {
+  static numCars = 0;
+
+  constructor(make, model, year, miles = 0) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+
+    Car.numCars += 1;
+  }
+
+  drive(distance) {
+    this.miles += distance;
+  }
+
+  static getNumCars() {
+    return Car.numCars;
+  }
 }
 
 const myNewCar = new Car('Nissan', 'Sentra', 2014);
+const myNewCar2 = new Car('Nissan', 'Sentra', 2014);
+const myNewCar3 = new Car('Nissan', 'Sentra', 2014);
+const myNewCar4 = new Car('Nissan', 'Sentra', 2014);
+const myNewCar5 = new Car('Nissan', 'Sentra', 2014);
 
 // Instance variables
 console.log(myNewCar.make); // output: 'Nissan'
@@ -38,8 +79,5 @@ myNewCar.drive(100);
 console.log(myNewCar.miles); // output: 100
 // Static method (on Car class, static, stays the same)
 console.log(Car.getNumCars()); // output: 1
-
-/* --------------------------- ES6 Class Construction --------------------------------------- */
-
 
 /* --------------------------- TypeScript Class Construction --------------------------------------- */
