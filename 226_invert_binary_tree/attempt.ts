66854fffa8694138a2bd88ab42e7836332fd5549
jcahela <jpacahela@gmail.com>
2023-08-23 17:46:36 -0500

    Invert binary tree practice with temp var

--- 226_invert_binary_tree/attempt.ts
+++ 226_invert_binary_tree/attempt.ts
@@ -53,3 +53,49 @@ var invertTree = function(root) {
 
   return root;
 };
+
+/************* Attempt #2 unsuccessful **************/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+
+
+I could use a dfs that switches the left and right subtrees while propogating up the recursive call stack
+
+                     me(4)
+                   /     \ 
+                 (7)     (2)
+                / \      /  \
+              (9) (6)   (3) (1)
+
+
+Pseudocode:
+
+
+Time complexity: O() - 
+Space complexity: O() - 
+
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (root === null) return root;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  const tmp = root.right;
+  root.right = root.left;
+  root.left = tmp;
+
+  return root;
+};
