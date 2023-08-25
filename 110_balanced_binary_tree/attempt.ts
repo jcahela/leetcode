151ba64e3e157c4454576a61a74b3c6338ddfe50
jcahela <jpacahela@gmail.com>
2023-08-25 16:53:32 -0500

    Balanced binary tree using height calculation and external boolean variable tracking

--- 110_balanced_binary_tree/attempt.ts
+++ 110_balanced_binary_tree/attempt.ts
@@ -81,3 +81,68 @@ function isBalanced(root: TreeNode | null): boolean {
 
   return balanced;
 };
+
+/******************* Attempt #2 - easy, good, dfs method **********************/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+So, this means for each subtree of each node, I need to find the subtree's depth, and make sure they don't differ by more than 1
+
+I could use recursive DFS to traverse every node
+
+Then, as I reach the base case, return 0
+
+For the recursive case, the depth of the current node is the max between left and right's depths
+
+However, before I return that, I need to check if left's depth is over 1 different than right
+    const isUnbalanced = Math.abs(leftDepth - rightDepth) > 1
+    set an outside var to isUnbalanced
+
+Then, I return the depth of the current tree (Math.max(leftdepth, rightDepth)) + 1
+
+Pseudocode:
+1. Instantiate a isUnbalanced boolean var at false
+2. Define a dfs function that takes in a node parameter
+    // Base case
+    1. If root is null, return 0
+    2. Instantiate a leftDepth at dfs(node.left)
+    3. Instantiate a rightDepth at dfs(node.right)
+    4. if (Math.abs(leftDepth - rightDepth) > 1) isUnbalanced = true
+    5. Return Math.max(leftDepth, rightDepth) + 1;
+3. Call dfs on root
+4. Return isUnabalanced 
+
+Time complexity: O(n) - dfs traversal time complexity
+Space complexity: O(h) - dfs traversal space complexity
+
+ */
+
+function isBalanced(root: TreeNode | null): boolean {
+    let balanced = true;
+    function dfs(node) {
+        if (node === null) return 0;
+
+        const leftDepth = dfs(node.left);
+        const rightDepth = dfs(node.right);
+
+        if (Math.abs(leftDepth - rightDepth) > 1) balanced = false;
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    dfs(root);
+    return balanced;
+};
