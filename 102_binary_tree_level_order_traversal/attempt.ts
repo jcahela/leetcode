339170160f5d227f002801b639c07ebd550474d5
jcahela <jpacahela@gmail.com>
2023-08-26 15:19:07 -0500

    Binary tree level order traversal practice using queue and level buckets

--- 102_binary_tree_level_order_traversal/attempt.ts
+++ 102_binary_tree_level_order_traversal/attempt.ts
@@ -86,3 +86,71 @@ function levelOrder(root: TreeNode | null): number[][] {
   }
   return output;
 };
+
+/********************** Attempt #2 easy using queue BFS *********************/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+
+I will need to do BFS, and have a bucket for each level, then once I reach a deeper level, push the existing bucket from the previous level to the output array, and clear the level bucket
+
+If I use a queue data structure on this tree
+
+            (3)
+           /   \
+         (9)   (20)
+        /   \   /  \
+     (10) (14)(15) (7)
+
+[ [(3), 1], [(9), 2],[(20), 2], [(10), 3], [(14), 3], [(20), 3], [(7), 3] ]
+
+At the end of checking a node, if the next node's level is 1 more than the current node's level, then I'm at the end of the current level, so I can push the level bucket to the output array
+
+Pseudocode:
+1. Instantiate an output array at []
+2. Instantiate a level array at []
+3. Instantiate a queue array at [root, 1], give it type [TreeNode, number][]
+4. While queue.length
+    1. Shift the first node-level tuple from the front of the array and store in val and level vars
+    2. push the current node's val into the level array
+    3. if the first node in queue's level is 1 more than the current level
+        1. Push level array onto output array
+        2. Set level array to be []
+    4. Add the current node's left child if exists onto the queue with its level + 1 of the current level
+    5. Add the current node's right child if exists onto the queue with its level + 1 of the current level
+5. Return output array
+
+Time complexity: O(n) - BFS
+Space complexity: O(n) - BFS queue
+
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) return [];
+  const output = [];
+  let levelBucket = [];
+  const queue: [TreeNode, number][] = [[root, 1]];
+
+  while (queue.length) {
+      const [node, level] = queue.shift();
+      levelBucket.push(node.val);
+      if (!queue.length || queue[0][1] === level + 1) {
+          output.push(levelBucket);
+          levelBucket = [];
+      }
+      if (node.left) queue.push([node.left, level + 1]);
+      if (node.right) queue.push([node.right, level + 1]);
+  }
+
+  return output;
+};
