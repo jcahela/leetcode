b96eddd5962c1eff98a80d53a24bcb31fa7b5b05
jcahela <jpacahela@gmail.com>
2023-08-26 22:56:57 -0500

    Validate binary search tree practice using min and max, dfs helper function, recursing out as soon as it invalidates

--- 98_validate_binary_search_tree/attempt.ts
+++ 98_validate_binary_search_tree/attempt.ts
@@ -31,3 +31,63 @@ At each node, I will have to ask:
   
       return isNodeValid && leftIsValid && rightIsValid;
   };
+
+/****************** Attempt #2 - easier use of min max values ******************/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+
+I need to keep track of mins and maxs as I traverse down the tree
+
+Since the left subtree must all have values less than the current node, the current node's val becomes the left subtree's max, and that keeps changing as you recurse left and right down the subtrees
+
+min can start as -Infinity
+max can start as Infinity
+
+if the current node is not between min and max, set isValid external variable to false
+
+set base case to check isValid is false, if it is, return up the stack
+
+when you recurse left, replace the max with the current node's val
+when you recurse right, replace the min with the current node's val
+
+Pseudocode:
+1. Instantiate isValid var at true
+2. Define function dfs that takes in node, min, and max
+    // Base case
+    1. if node is null or isValid is false, return
+    // Recursive case
+    2. if node's val is less than min or greater than max, set isValid to false
+    3. recurse left, replace max with current node's val
+    4. recurse right, replace min with current node's val
+3. Call dfs on root, with min being -Infinity, and max being Infinity
+4. Return isValid
+
+Time complexity: O(n) - DFS time complexity
+Space complexity: O(h) - DFS space
+
+ */
+
+function isValidBST(root: TreeNode | null): boolean {
+    let isValid = true;
+    function dfs(node: TreeNode | null, min: number, max: number) {
+        if (node === null || !isValid) return;
+
+        if (node.val <= min || node.val >= max) isValid = false;
+
+        dfs(node.left, min, node.val);
+        dfs(node.right, node.val, max);
+    }
+    dfs(root, -Infinity, Infinity);
+    return isValid;
+};
