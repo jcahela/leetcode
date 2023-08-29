9e0910877d0a17a662977a335a24dcafba654053
jcahela <jpacahela@gmail.com>
2023-08-29 15:03:30 -0500

    Car fleet practice with stack of cars

--- 853_car_fleet/attempt.js
+++ 853_car_fleet/attempt.js
@@ -85,3 +85,89 @@ var carFleet = function(target, position, speed) {
   return carFleets;
   
 };
+
+/******************* Attempt #2 - easy ********************/
+
+/*
+
+I need to calculate the time to destination for each car
+
+If a car in front has a time to destination greater than the car behind it, the car behind it will form a fleet with it
+
+I can use a stack and order the cars from closest-to-destination to furthest-from-destination, then check if the top car will form a fleet with the car behind it (has a >= time to destination than the car behind it). If it does, I can pop out the car behind the top car, and put the top car back onto the stack, since it's now 1 fleet with the car behind it
+
+If it has a smaller time to destination than the car behind it, that means the top car will be its own fleet, so I can add 1 to an output var
+
+At the end of checking all the cars in the stack, I can add th remaining cars in the stack (cars that have formed fleets with cars behind it) to the output var, to get the total car fleets
+
+[10,8,0,5,3]
+[2,4,1,1,3]
+
+Target = 12
+
+Speed + position:
+[[10,2], [8,4], [0,1], [5,1], [3,3]]
+
+Sorted speed + position:
+[[0,1]]
+
+output = 2
+
+0. Pop the top car from the stack
+1. Does the popped car have a smaller time to destination than the top car (car behind it)?
+    1. If so, it won't form a fleet, so keep the popped car out of the stack and add 1 to output
+    2. If not, it will form a fleet either at or before the destination, so pop the next car from the stack, and push the first popped car back onto the stack
+
+Keep checking the stack until it's got 1 left
+
+Return output + 1
+
+Pseudocode:
+1. Instantiate an output var at 0
+2. Instantiate a positionSpeed array at []
+3. Iterate over position array
+    1. Add the current position and the current speed to the positionSpeed array as a tuple
+4. Instantiate a sorted array at positionSpeed.sort((a,b) => a[0] - b[0])
+5. while the sorted stack has a length greater than 1
+    1. Instantiate a topCar var at sorted.pop()
+    2. Instantiate a nextCar var at sorted.pop()
+    3. Instantiate a topCarTTD var at (target - topCar[0]) / topCar[1]
+    4. Instantiate a nextCarTTD var at (target - nextCar[0]) / topCar[1]
+    5. Check if topCarTTD is smaller than nextCarTTD
+        1. If so,
+            1. It won't form a fleet, so add nextCar back to sorted
+            2. Add 1 to the output var
+        2. If not
+            1. It will form a fleet with the top car, so add topCar back to sorted
+6. Return output + 1
+
+Time complexity: O(n log n) - Since we have to sort the cars based on their position in order to calculate the TTD in the right order for determining car fleets
+Space complexity: O(n) - Where n is the number of cars being evaluated, since we store a 2D array of n tuples to create the stack to verify TTD on
+
+*/
+
+// [ [ 0, 1 ] ]
+
+function carFleet(target: number, position: number[], speed: number[]): number {
+    let output = 0; // 2
+    const positionSpeed = [];
+    for (let i = 0; i < position.length; i += 1) {
+        positionSpeed.push([position[i], speed[i]]);
+    }
+    const sorted = positionSpeed.sort((a,b) => a[0] - b[0]);
+
+    while (sorted.length > 1) {
+        const topCar = sorted.pop(); // [ 5, 1 ] 
+        const nextCar = sorted.pop(); // 
+        const topCarTTD = (target - topCar[0]) / topCar[1]; // (12 - 5) / 1 = 7
+        const nextCarTTD = (target - nextCar[0]) / nextCar[1]; // (12 - 0) / 1 = 12
+
+        if (topCarTTD < nextCarTTD) {
+            sorted.push(nextCar);
+            output += 1;
+        } else {
+            sorted.push(topCar);
+        }
+    }
+    return output + 1;
+};
