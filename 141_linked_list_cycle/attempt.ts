d5dabab67ebcec709ee45964f9b8995e11dcccb0
jcahela <jpacahela@gmail.com>
2023-08-29 15:29:13 -0500

    linked list cycle using floyd's practice

--- 141_linked_list_cycle/attempt.ts
+++ 141_linked_list_cycle/attempt.ts
@@ -48,3 +48,29 @@ function hasCycle(head: ListNode | null): boolean {
   
   return false;
 };
+/****************** Attempt #3 - practice writing out floyd's algorithm, easy *******************/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) return true;
+    }
+
+    return false;
+};
