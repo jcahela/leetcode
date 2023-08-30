e916209f5e184d0cfd43cc0c2d01b37a0d28434a
jcahela <jpacahela@gmail.com>
2023-08-30 14:39:09 -0500

    Merge two sorted list practice

--- 21_merge_two_sorted_lists/attempt.ts
+++ 21_merge_two_sorted_lists/attempt.ts
@@ -175,3 +175,26 @@ function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode
 
     return dummy.next;
 };
+
+/**************** Attempt #4 easy done in 1 minute *******************/
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummy = new ListNode();
+    let tail = dummy;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            tail.next = list1;
+            list1 = list1.next;
+        } else {
+            tail.next = list2;
+            list2 = list2.next;
+        }
+        tail = tail.next
+    }
+
+    if (list1) tail.next = list1;
+    if (list2) tail.next = list2;
+
+    return dummy.next;
+};
