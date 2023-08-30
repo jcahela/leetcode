6732019a65e435b8b376d1032a319d24d28998fd
jcahela <jpacahela@gmail.com>
2023-08-30 17:24:55 -0500

    Combination sum practice, not successful

--- 39_combination_sum/attempt.ts
+++ 39_combination_sum/attempt.ts
@@ -17,6 +17,34 @@ I could start with 0 as the root condition
 
 */
 
-function combinationSum(candidates: number[], target: number): number[][] {
+/************* Attempt #2 - wasn't sure on the decision tree or how to start *************/
+/*
 
+Practiced writing out solution and understanding approach
+
+*/
+
+function combinationSum(candidates: number[], target: number): number[][] {
+    const output = [];
+
+    function backtrack(i: number, curr: number[], total: number) {
+        if (total === target) {
+            output.push([...curr]);
+            return;
+        }
+
+        if (i >= candidates.length || total > target) {
+            return;
+        }
+
+        curr.push(candidates[i]);
+        backtrack(i, curr, total + candidates[i]);
+
+        curr.pop();
+        backtrack(i + 1, curr, total);
+    }
+
+    backtrack(0, [], 0);
+
+    return output;
 };
