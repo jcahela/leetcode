a74263b9db9a6aac99304f4ba3693cde11f010e6
jcahela <jpacahela@gmail.com>
2023-09-06 16:12:11 -0500

    Right side view using BFS and queue data structure

--- 199_binary_tree_right_side_view/attempt.ts
+++ 199_binary_tree_right_side_view/attempt.ts
@@ -79,3 +79,67 @@ function rightSideView(root: TreeNode | null): number[] {
 
     return output;
 };
+
+/*********************** Attempt #3 - easy, bfs using queue data structure *******************************/
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+
+Idea: use breadth first search, grab the last node of each level, and push their values into the output array
+
+When are you on the last node of a level?
+    - If I store the levels of the nodes I put into the queue, I could check if the next node's level is 1 more than the current node i'm looking at. If that's true, then the current node I'm looking at is the last node of its level, since I'm using FIFO structure (queue)
+
+Pseudocode:
+1. Instantiate a queue array that'll hold a tuple of treenode and number noting its level: [TreeNode, number][], and put the root node in it with the number 1 as its second value
+2. Instantiate an output array that'll hold numbers (values of the right-side nodes)
+3. While the queue has a length
+    1. Shift the first node from the front of the queue and store in a curr var
+    2. If the queue has no length OR the first node in the queue has a level greater than the current node's level
+        1. Add the current node's value to the output array
+    3. If the current node has a left child, add it to the queue with its level being the current level + 1
+    4. If the current node has a right child, add it to the queue with its level being the current level + 1
+4. Return output array
+
+Time: O(n) - Since I'm traversing the list BFS style
+Space: O(n) - Since the binary tree could be unbalanced and n-1 nodes could be children of root
+
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    if (!root) return [];
+    const queue: [TreeNode, number][] = [[root, 1]];
+    const output = [];
+
+    while (queue.length) {
+        const [curr, level] = queue.shift();
+        console.log(curr);
+        if (!queue.length || queue[0][1] > level) output.push(curr.val);
+        if (curr.left) queue.push([curr.left, level + 1]);
+        if (curr.right) queue.push([curr.right, level + 1]);
+    }
+
+    return output;
+};
+
+/*
+
+[[(5), 3], [(4) , 3]]
+
+           1
+         /   \
+        2     3
+         \      \
+           5     4
+
+*/
