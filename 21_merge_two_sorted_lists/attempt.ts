fcac34a700ee1610a199bcecf3ac18d10bb69e8c
jcahela <jpacahela@gmail.com>
2023-09-20 13:04:41 -0500

    Merge two sorted lists linked list and pointer practice

--- 21_merge_two_sorted_lists/attempt.ts
+++ 21_merge_two_sorted_lists/attempt.ts
@@ -198,3 +198,26 @@ function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode
 
     return dummy.next;
 };
+
+/****************** Attempt #5 - easy, forgot to increment list1 and list2 in the while loop, but remembered after *****************/
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummy = new ListNode();
+    let tail = dummy;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            tail.next = list1;
+            list1 = list1.next;
+        } else {
+            tail.next = list2;
+            list2 = list2.next;
+        }
+        tail = tail.next;
+    }
+
+    if (list1) tail.next = list1;
+    if (list2) tail.next = list2;
+
+    return dummy.next;
+};
